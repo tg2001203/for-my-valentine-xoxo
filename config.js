403efea73a4efbd9c67ad6b7768b8bd9cf8d2c74
tg2001203 <ghoshtania851@gmail.com>
2025-02-07 23:01:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -50,8 +50,8 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        message: "Now come get your gift, a big warm hug and endless kisses!",
+        emojis: "💋💋💋💋💋💋💋💋💋💋💋💋💋"  // These will bounce around
     },
 
     // Color scheme for the website
